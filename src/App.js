2bc38917b2485e1c75c03947b193a07e5594ddcc
Prabal Bhandari <54536906+prabalbhandari04@users.noreply.github.com>
2023-12-23 09:50:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import Card from "./components/Cards";
+import Carousel from "./components/Carousel";
+
+function App() {
+  let cards = [
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
+      )
+    }
+  ];
+  return (
+    <div className="">
+      <Carousel
+        cards={cards}
+        height="500px"
+        width="30%"
+        margin="0 auto"
+        offset={2}
+        showArrows={false}
+      />
+    </div>
+  );
+}
+
+export default App;
